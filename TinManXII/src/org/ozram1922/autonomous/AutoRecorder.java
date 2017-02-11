7bcd8db5f50885fca505fd73b414f39e3f7bbf4c
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-02-11 14:40:40 -0500

    Added Data Trimming Feature to Recorder
    Now after recording, the data can be trimmed so the setpoints at the
    beginning and end of the recording that are very low velocity or
    position deltas can be removed.

--- TinManXII/src/org/ozram1922/autonomous/AutoRecorder.java
+++ TinManXII/src/org/ozram1922/autonomous/AutoRecorder.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class AutoRecorder {
 	
 	private ArrayList<Long> _timeTable = new ArrayList<Long>();
-	private ArrayList<Double> _distanceTable = new ArrayList<Double>();
+	private ArrayList<Double> _valueTable = new ArrayList<Double>();
 	private long _nanoTimeOffset = 0;
 	
 	public void StartRecording()
@@ -17,7 +17,40 @@ public class AutoRecorder {
 	public void Update(double newVal)
 	{
 		_timeTable.add(System.nanoTime() - _nanoTimeOffset);
-		_distanceTable.add(newVal);
+		_valueTable.add(newVal);
+	}
+	
+	//this helps to remove noise in the beginning and end of the data
+	public void TrimVelocityData(double threshold)
+	{
+		//remove leading and trailing small values
+		
+		int startIndex = 0;
+		for(; 
+			(startIndex < _valueTable.size()) || (_valueTable.get(startIndex) < threshold); 
+			++startIndex);
+		
+		int endIndex = _valueTable.size() - 1;
+		for(;
+			(endIndex > startIndex) || (_valueTable.get(endIndex) < threshold);
+			--endIndex);
+		
+		_valueTable = (ArrayList<Double>) _valueTable.subList(startIndex, endIndex);
+	}
+	
+	public void TrimPositionData(double deltaThreshold)
+	{
+		//removing leading and trailing 0 deltas
+		
+		int startIndex = 0;
+		for(;
+			(startIndex < _valueTable.size() - 1) || (Math.abs(_valueTable.get(startIndex) - _valueTable.get(startIndex + 1)) < deltaThreshold);
+			++startIndex);
+		
+		int endIndex = _valueTable.size() - 1;
+		for(;
+			(endIndex > startIndex + 1) || (Math.abs(_valueTable.get(endIndex) - _valueTable.get(endIndex - 1)) < deltaThreshold);
+			--endIndex);
 	}
 	
 	public String Serialize()
@@ -27,7 +60,7 @@ public class AutoRecorder {
 		for(int i = 0; i < _timeTable.size(); ++i) {
 			sw.write(_timeTable.get(i).toString());
 			sw.write(",");
-			sw.write(_distanceTable.get(i).toString());
+			sw.write(_valueTable.get(i).toString());
 			sw.write("\n");
 		}
 		sw.flush();
