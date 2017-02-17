2c6fe1c78ba4c90b5eed7845f0384ab5e247cff3
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-02-17 18:18:03 -0500

    Added Encoder Recorders as Commands

---
+++ TinManXII/src/org/usfirst/frc/team1922/robot/commands/auto/RecordEncoderPosition.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team1922.robot.commands.auto;
+
+import org.ozram1922.autonomous.AutoRecorder;
+import org.usfirst.frc.team1922.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+/**
+ *
+ */
+public class RecordEncoderPosition extends Command {
+	
+	AutoRecorder _leftRecorder = new AutoRecorder();
+	AutoRecorder _rightRecorder = new AutoRecorder();
+	NetworkTable _entry;
+	int leftEntryNumber = 0;
+	int rightEntryNumber = 0;
+
+    public RecordEncoderPosition(String tableName) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	_entry = NetworkTable.getTable(tableName);
+    	while(_entry.containsKey(Integer.toString(leftEntryNumber) + "L")) leftEntryNumber++;
+    	while(_entry.containsKey(Integer.toString(rightEntryNumber) + "R")) rightEntryNumber++;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	_leftRecorder.StartRecording();
+    	_rightRecorder.StartRecording();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	_leftRecorder.Update(Robot.mDriveTrain.GetLeftPosition());
+    	_rightRecorder.Update(Robot.mDriveTrain.GetRightPosition());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	_entry.putString(leftEntryNumber + "L",_leftRecorder.Serialize());
+    	_entry.putString(rightEntryNumber + "R",_rightRecorder.Serialize());
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
