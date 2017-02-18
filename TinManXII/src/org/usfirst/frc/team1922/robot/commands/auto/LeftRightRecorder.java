ab9fb7f56652cd1ad0ab7e9173734bc269b78a17
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-02-17 20:43:03 -0500

    Modified Recorder to use Files Instead
    Now files are created and written to instead of network tables.  That
    method is kind of a pain to access.  Now FTP can be used to retrieve
    paths.

--- TinManXII/src/org/usfirst/frc/team1922/robot/commands/auto/LeftRightRecorder.java
+++ TinManXII/src/org/usfirst/frc/team1922/robot/commands/auto/LeftRightRecorder.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team1922.robot.commands.auto;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import org.ozram1922.autonomous.AutoRecorder;
 import org.usfirst.frc.team1922.robot.Robot;
 
@@ -13,18 +17,23 @@ public abstract class LeftRightRecorder extends Command {
 
 	AutoRecorder _leftRecorder = new AutoRecorder();
 	AutoRecorder _rightRecorder = new AutoRecorder();
-	NetworkTable _entry;
+	String subDir;
+	static String basePath = "/home/lvuser/TinManRecordings/";
 
     public LeftRightRecorder(String tableName) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	_entry = (NetworkTable) NetworkTable.getTable("T1922").getSubTable(tableName);
+    	//_entry = (NetworkTable) NetworkTable.getTable("T1922").getSubTable(tableName);
+    	subDir = tableName;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	_leftRecorder.StartRecording();
     	_rightRecorder.StartRecording();
+
+    	new File(basePath).mkdirs();
+    	new File(basePath + subDir).mkdirs();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -41,15 +50,59 @@ public abstract class LeftRightRecorder extends Command {
     protected boolean isFinished() {
         return false;
     }
+    
+    protected String GetFileName(int number, String modifier)
+    {
+    	return basePath + subDir + "/" + number + modifier + ".csv";
+    }
 
     // Called once after isFinished returns true
     protected void end() {
-    	int leftEntryNumber = 0;
-    	int rightEntryNumber = 0;
-    	while(_entry.containsKey(leftEntryNumber + "L")) leftEntryNumber++;
-    	while(_entry.containsKey(rightEntryNumber + "R")) rightEntryNumber++;
-    	_entry.putString(leftEntryNumber + "L",_leftRecorder.Serialize());
-    	_entry.putString(rightEntryNumber + "R",_rightRecorder.Serialize());
+    	int leftEntryNumber = -1;
+    	int rightEntryNumber = -1;
+    	//while(_entry.containsKey(leftEntryNumber + "L")) leftEntryNumber++;
+    	//while(_entry.containsKey(rightEntryNumber + "R")) rightEntryNumber++;
+    	//_entry.putString(leftEntryNumber + "L",_leftRecorder.Serialize());
+    	//_entry.putString(rightEntryNumber + "R",_rightRecorder.Serialize());
+    	
+    	//create the file structure if it doesn't exist
+    	
+    	//get the left file name
+    	File f;
+    	do
+    	{
+    		leftEntryNumber++;
+    		f = new File(GetFileName(leftEntryNumber, "L"));
+    	}
+    	while(f.exists() && !f.isDirectory());
+    	
+    	//get the right file name
+    	do
+    	{
+    		rightEntryNumber++;
+    		f = new File(GetFileName(rightEntryNumber, "R"));
+    	}
+    	while(f.exists() && !f.isDirectory());
+    	
+    	
+    	//output the left information
+    	try(PrintWriter out = new PrintWriter(GetFileName(leftEntryNumber, "L")))
+    	{
+    	    out.println(_leftRecorder.Serialize());
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    	
+    	//output the right information
+    	try(PrintWriter out = new PrintWriter(GetFileName(rightEntryNumber, "R")))
+    	{
+    	    out.println(_rightRecorder.Serialize());
+    	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     // Called when another command which requires one or more of the same
