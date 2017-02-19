0f44ae6a24675c988c67949514deba36cc1f6f36
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-02-18 22:32:53 -0500

    Simplified Vector Class
    This no longer uses the Vector Interface that never made it into the
    repo

--- TinManXII/src/org/ozram1922/fieldsense/Vector2d.java
+++ TinManXII/src/org/ozram1922/fieldsense/Vector2d.java
@@ -1,24 +1,12 @@
 package org.ozram1922.fieldsense;
 
-public class Vector2d implements Vector<double> {
-	
+public class Vector2d {	
 	public double x = 0;
 	public double y = 0;
 	
-	@Override
-	public double ValueAt(int index) throws Exception
+	public Vector2d(double X, double Y)
 	{
-		if(index == 0)
-			return x;
-		else if(index == 1)
-			return y;
-		else
-			throw new Exception("Index Out of Range");
-	}
-	
-	@Override
-	public int GetCount()
-	{
-		return 2;
+		x = X;
+		y = Y;
 	}
 }
