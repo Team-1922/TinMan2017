98f984eb64a1af48cc35fb0623d61a15df6d55da
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-03-07 17:21:48 -0500

    Fixed EncoderIntegrater
    THis makes the fix so that the Double.NaN does not propagate into the
    cumulative position/direction by removing the possibility for Double.NaN
    coming through the if statement.  Now, the first time through check is
    handled by a separate boolean.

--- TinManXII/src/org/ozram1922/fieldsense/EncoderIntegrater.java
+++ TinManXII/src/org/ozram1922/fieldsense/EncoderIntegrater.java
@@ -3,33 +3,39 @@ package org.ozram1922.fieldsense;
 import org.ozram1922.OzUtils;
 import org.ozram1922.Vector2d;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class EncoderIntegrater {
 	
 	private double _wheelSpacing;
-	private double _direction;
+	private double _direction = 0;
 	private double _time;
-	private Vector2d _position;
+	private Vector2d _position = new Vector2d();
 	
 
-	private double _prevTime;
-	private Vector2d _prevPosition;
+	private double _prevTime = 0;
+	private Vector2d _prevPosition = new Vector2d();
 	
-	private double _prevL = Double.NaN;
-	private double _prevR = Double.NaN;
+	private double _prevL = 0;
+	private double _prevR = 0;
+	
+	private boolean _isFirstTime = true;
 	
 	public EncoderIntegrater(double wheelSpacing, Vector2d initialPosition)
 	{
 		_wheelSpacing = wheelSpacing;
-		_position = initialPosition;
+		_position.Set(initialPosition);
 	}
 	
 	public void Cycle(double L, double R)
 	{
 		//acount for the first cycle
-		if(_prevL == Double.NaN)
+		if(_isFirstTime)
 		{
+			_isFirstTime = false;
 			_prevL = L;
 			_prevR = R;
+			_prevTime = OzUtils.GetTime();
 			return;
 		}
 		double dL = L - _prevL;
@@ -37,6 +43,7 @@ public class EncoderIntegrater {
 		
 		if(dL == 0 && dR == 0)
 			return;
+		
 		
 		//the change in left and right are actually the arc lengths of two concentric circles since encoder units are 
 		//	essential continuous
@@ -58,14 +65,16 @@ public class EncoderIntegrater {
 		double deltaDirection = (bigChange - smallChange) / _wheelSpacing;
 		double deltaPositionMagnitude = 2.0 * (_wheelSpacing / 2.0 + smallChange / deltaDirection) * Math.sin(deltaDirection / 2.0);
 		
+		
 		//update the previous position
-		_prevPosition = _position;
+		_prevPosition.Set(_position);
 		_prevTime = _time;
 		
 		_direction += deltaDirection;
-		_position.x += deltaPositionMagnitude * Math.cos(deltaDirection);
-		_position.y += deltaPositionMagnitude * Math.sin(deltaDirection);
+		_position.x += deltaPositionMagnitude * Math.cos(_direction);
+		_position.y += deltaPositionMagnitude * Math.sin(_direction);
 		_time = OzUtils.GetTime();
+		
 		
 		//set the previous to the current
 		_prevR = R;
