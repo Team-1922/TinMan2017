3d73f4e0d009b12f97de28b26b601ef8c7ad6669
Kevin Mackenzie <dev.kjmack305@gmail.com>
2017-04-04 22:25:38 -0400

    Added Straight Peg

---
+++ TinManXII/src/org/usfirst/frc/team1922/robot/commands/auto/StraightPeg.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team1922.robot.commands.auto;
+
+import org.usfirst.frc.team1922.robot.commands.CloseGearFlap;
+import org.usfirst.frc.team1922.robot.commands.OpenGearFlap;
+import org.usfirst.frc.team1922.robot.commands.TimedTankDrive;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class StraightPeg extends CommandGroup {
+	
+    public StraightPeg() {
+    	
+    	//drive forward
+    	addSequential(new TimedTankDrive(0.75, 0.75, 1));
+    	
+    	//Enable vision tracking
+    	addSequential(new SetVisionTrackingState(true));
+    	
+    	//drive forwards towards the peg
+    	addSequential(new TimedTankDrive(0.4, 0.4, 3));
+    	
+    	//during the previous 3 seconds, hopefully the bot has had a chance to correct
+    	addSequential(new SetVisionTrackingState(false));
+    	
+    	//continue driving into the peg
+    	addSequential(new TimedTankDrive(0.4, 0.4, 2));
+    	
+    	//drop off the gear ...
+    	addParallel(new OpenGearFlap());
+    	//... while driving backwards
+    	addSequential(new TimedTankDrive(-0.75, -0.75, 1));
+    	
+    	//close the gear flap
+    	addSequential(new CloseGearFlap());
+    }
+}
